29229ed64e92ffa410740bdea2b6018443611fdd
209440378 <gimlet0214999@gmail.com>
2024-07-03 14:08:10 +0800

    0703

---
+++ hm_02.java
@@ -0,0 +1,11 @@
+public class hm_02 {  
+    public static void main(String[] args){
+        String name="陳昶宏";
+        String age="26";
+        String hobby="旅遊";
+
+        System.out.println("姓名:"+name);
+        System.out.println("年齡:"+age);
+        System.out.println("興趣愛好:"+hobby);
+    }
+}
